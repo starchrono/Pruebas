6c80539f5ae917afa4eff316b91a79b6e71fb78f
starchrono <starchrono@outlook.com>
2015-11-06 00:56:13 -0600

    Ocho

---
+++ my-app/www/XD.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Clientes</title>
+    <meta charset="utf-8">
+    <link type="text/css" href="style2.css" rel="stylesheet" />
+	<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Philosopher:700italic' rel='stylesheet' type='text/css'>
+</head>
+
+      <table>
+					<tr>
+						<td class="first">
+                <form id="form-login"  method="post" autocomplete="off" action="index.php">
+
+
+                    
+					</tr>
+				</table>
+
+				
+                    <p id="bot"><input name="submit" type="submit" id="boton" value=":P" class="boton"/></p>
+                   
+                    
+                </form>
+
+
+</html>
